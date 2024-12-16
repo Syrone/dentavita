aa44c15fb6210d1386976de8dd3f23cec3128d6a
Andrew Vasilchenko <cazonove@gmail.com>
2024-12-16 23:38:26 +0300

    added section trust

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,4 +1,37 @@
-// import Swiper from 'swiper';
-// import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
 
-// Swiper.use([Navigation, Pagination]);
+Swiper.use([Navigation, Pagination])
+
+document.querySelectorAll('.trust-swiper')?.forEach(element => {
+	new Swiper(element.querySelector('.swiper'), {
+		slidesPerView: 5,
+		spaceBetween: 16,
+		pagination: {
+			el: element.querySelector('.swiper-pagination'),
+		},
+
+		breakpoints: {
+			0: {
+				slidesPerView: 1.25,
+				spaceBetween: 10,
+			},
+			576: {
+				slidesPerView: 2,
+				spaceBetween: 10,
+			},
+			768: {
+				slidesPerView: 3,
+				spaceBetween: 10,
+			},
+			992: {
+				slidesPerView: 4,
+				spaceBetween: 16,
+			},
+			1200: {
+				slidesPerView: 5,
+				spaceBetween: 16,
+			}
+		}
+	})
+})
